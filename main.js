5534fc72afbbeef7d410f837858feafe467e1823
Yerko <40376428+Purpplee@users.noreply.github.com>
2022-09-28 18:29:46 -0300

    add jquery

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
         function(data){
             $.each(data, function(i, item){
                 $('#table').append("<tr><td>" + item.id + "</td><td>" + item.nombre + "</td><td>"
-                + item.email + "</td><td>" + item.edad + "</td><td><img src=" + item.avatar + "></td><td><button id='borrar' class='borrar' type='button'>eliminar</button>" + "</td></tr>");
+                + item.email + "</td><td>" + item.edad + "</td><td><img src=" + item.avatar + "></td><td><button id='borrar' class='borrar' type='button'>eliminar</button>" + "<button type='button' id='modalButton' class='btn btn-primary' data-toggle='modal' data-target='#exampleModal'>info</button>" + "</td></tr>");
             });
 
             $('.borrar').click(function(){
@@ -18,12 +18,27 @@ $(document).ready(function(){
                 eliminar[index].remove();
                 console.log(index);
             });
+
+            $('#modalButton').click(function(){
+                var info = $.get('https://my-json-server.typicode.com/alaravena/ldp3101/usuarios',
+                function(data){
+                    $.each(data, function(i, item){
+                        $('.modal-text').append(item.id + '<br>' 
+                        + item.nombre + '<br>' 
+                        + item.email + '<br>' 
+                        + item.edad + '<br>').index(this)
+
+
+                        console.log(item)
+                    });
+                });
+
         });
     });
 
 
 });
-
+});
 
 
 
