701b11be2eb0316ba7bec03c4258bd723553c38c
Yerko <ramirezyerko566@gmail.com>
2022-10-04 19:32:00 -0300

    new

--- main.js
+++ main.js
@@ -8,18 +8,31 @@ $(document).ready(function(){
         function(data){
             $.each(data, function(i, item){
                 $('#table').append("<tr><td>" + item.id + "</td><td>" + item.nombre + "</td><td>"
-                + item.email + "</td><td>" + item.edad + "</td><td><img src=" + item.avatar + "></td><td><button id='borrar' class='borrar' type='button'>eliminar</button>" + "<button type='button' id='modalButton' class='btn btn-primary' data-toggle='modal' data-target='#exampleModal'>info</button>" + "</td></tr>");
+                + item.email + "</td><td>" + item.edad + "</td><td><img src=" + item.avatar + "></td><td><button id='borrar' class='borrar' type='button'>eliminar</button>" + "<button type='button' class='modalButton btn btn-primary' data-toggle='modal' data-target='#exampleModal'>info</button>" + "</td></tr>");
+
+                $('.borrar').click(function(){
+                    var index = $('.borrar').index(this);
+                    var eliminar = $('tr');
+            
+                    eliminar[index].remove();
+                    console.log(index);
+                });
+
+
+                
+    
+                var modal = $('.modal-body')
+       
+    
+                $('.modalButton').click(function(){
+                    $('tr').show(modal.text(item.id + ' ' + item.nombre + ' ' + item.email + ' ' + item.edad));
+                })
+
+                
             });
 
-            $('.borrar').click(function(){
-                var index = $('.borrar').index(this);
-                var eliminar = $('tr');
-        
-                eliminar[index].remove();
-                console.log(index);
-            });
 
-            $('#modalButton').click(function(){
+            /*$('#modalButton').click(function(){ 
                 var info = $.get('https://my-json-server.typicode.com/alaravena/ldp3101/usuarios',
                 function(data){
                     $.each(data, function(i, item){
@@ -33,7 +46,7 @@ $(document).ready(function(){
                     });
                 });
 
-        });
+        });*/
     });
 
 
